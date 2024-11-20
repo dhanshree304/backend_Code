3c81b13a595b8a85556883f5a1b98c9ef6a1a220
dhanshree304 <dhanshreeupadhye0@gmail.com>
2024-11-20 17:38:34 +0530

    nodeJs basics

---
+++ node-js-1/cacl.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+
+module.exports = { add, sub, mul, div };
+
+
+//u exports the fun in obj becoz order does not matter.
