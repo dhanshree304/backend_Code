05ed211bcbca9f42c65fdde8336e3335274441cf
dhanshree304 <dhanshreeupadhye0@gmail.com>
2024-11-20 23:11:49 +0530

    node-js

--- node-js-1/1.js
+++ node-js-1/1.js
@@ -2,20 +2,20 @@
 
 
 
-const {add,mul,sub,div} = require("./cacl");
-//here u import in same obj in any order..import funs and file both
-console.log(add(3,4));
-console.log(sub(3,1));
-console.log(mul(3,4));
-console.log(div(20,4));
+// const {add,mul,sub,div} = require("./cacl");
+// //here u import in same obj in any order..import funs and file both
+// console.log(add(3,4));
+// console.log(sub(3,1));
+// console.log(mul(3,4));
+// console.log(div(20,4));
 
- console.log("hello");//1
+//  console.log("hello");//1
 
-const fs = require("fs")
-const data = fs.readFileSync("./cacl.js",{encoding:"utf-8"})
-console.log(data);//2
+// const fs = require("fs")
+// const data = fs.readFileSync("./cacl.js",{encoding:"utf-8"})
+// console.log(data);//2
 
-console.log("Hope u read the file")//3
+// console.log("Hope u read the file")//3
 
 const checkEven=require("is-even")
 console.log(checkEven(7))
